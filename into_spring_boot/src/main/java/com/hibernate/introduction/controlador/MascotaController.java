fd6f0cadd28e97971621ed562f4b5a7439157e52
Rg-bit <rjim3nez@gmail.com>
2022-09-14 07:06:23 -0500

    ordenar con MVC

---
+++ into_spring_boot/src/main/java/com/hibernate/introduction/controlador/MascotaController.java
@@ -0,0 +1,53 @@
+package com.hibernate.introduction.controlador;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hibernate.introduction.modelo.Mascota;
+import com.hibernate.introduction.servicios.MascotaService;
+
+@RestController
+@RequestMapping("/mascotas")
+public class MascotaController {
+
+  private MascotaService service;
+
+  // CONSTRUCTOR
+  public MascotaController() {
+    service = new MascotaService();
+  }
+
+  /*
+   * @GetMapping
+   * public String getMascotas() {
+   * return "Hola mundo utilizando Spring Boot";
+   * }
+   */
+
+  @GetMapping
+  public List<Mascota> getMascotas() {
+    return service.getMascotas();
+  }
+
+  @GetMapping("/{raza}")
+  public List<Mascota> getXRaza(@PathVariable(name = "raza") String raza) {
+    return service.getXRaza(raza);
+  }
+
+  @GetMapping("/commons")
+  public String getNombreApellidoComun(@RequestParam String nombre, @RequestParam String apellido) {
+    return service.getNombreApellidoComun(nombre, apellido);
+  }
+
+  @PostMapping
+  public String create(@RequestBody Mascota mascota) {
+    return service.create(mascota);
+  }
+
+}
