6ada674fb489d160e63503cf3156f25046383012
Rg-bit <rjim3nez@gmail.com>
2022-09-14 09:48:50 -0500

    integracion con proyecto spring boot

---
+++ veterinaria/scripts/formulario.js
@@ -0,0 +1,6 @@
+function get_data_form (evt) {
+    // Indicar que no recarge página al enviar el formulario
+    evt.preventDefault()
+    let nombre = evt.target.nombre.value;
+    console.table({ nombre })
+  }
